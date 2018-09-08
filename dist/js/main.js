573f1c4945a10320b7da6a95bd52b0d0284a199f
tthtnt <2420313474@qq.com>
2018-09-08 15:49:50 +0800

    主页功能基本完成

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   getSize();
+  // disabledMouseWheel();
   my$("open-news-directory").onclick = function () {
     changeAnimate(my$("news-directory-content"),{"right":"-450"},function () {
       changeAnimate(my$("news-directory-close"),{"left":"450"});
@@ -10,6 +11,27 @@ window.onload = function () {
       changeAnimate(my$("news-directory-content"),{"right":"0"});
     })
   };
+  let news_details = my$("news-detail").getElementsByClassName("news-details");
+  for (let key in news_details) {
+    news_details[key].onclick = function () {
+      changeAnimate(my$("news-contents-inner"),{"right":"0"},function () {
+        changeAnimate(my$("news-contents-close"),{"right":"900"})
+      })
+    }
+  }
+  my$("news-contents-close").onclick = function () {
+    changeAnimate(my$("news-contents-close"),{"right":"-60"},function () {
+      changeAnimate(my$("news-contents-inner"),{"right":"-900"})
+    })
+  };
+  my$("video_under").onclick = function () {
+    let height = getViewPortHeight();
+    changeAnimate(window,{"scrollTop":"-"+height})
+  }
+  let introduction = $("#introduction").offset().top;
+  $("#video_under").click(function () {
+    $('html,body').animate({scrollTop:introduction},1000);
+  })
 };
 
 function getSize() {
@@ -30,4 +52,25 @@ function getSize() {
 function changeSize(dv, width, height) {
   dv.style.width = width + "px";
   dv.style.height = height + "px";
+}
+
+
+function disabledMouseWheel() {
+  if(document.addEventListener) {
+    document.addEventListener('DOMMouseScroll', scrollFunc, false);
+  }//W3C
+  window.onmousewheel = document.onmousewheel = scrollFunc;//IE/Opera/Chrome
+}
+function scrollFunc(evt) {
+  evt = evt || window.event;
+  if (evt.preventDefault) {
+    // Firefox
+    evt.preventDefault();
+    evt.stopPropagation();
+  } else {
+    // IE
+    evt.cancelBubble = true;
+    evt.returnValue = false;
+  }
+  return false;
 }
